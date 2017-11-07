a1318902872b7b539c5e5bcaa0b4ea872403fb39
David Jones <djones6@uk.ibm.com>
2017-11-07 15:27:48 +0000

    Add Swift 4 as a language dependency

--- frameworks/Swift/kitura-net/setup.sh
+++ frameworks/Swift/kitura-net/setup.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
 # Dependency on PostgreSQL database
-fw_depends postgresql
-
-# Install Swift dependencies
-sudo apt-get -y install clang-3.8 libicu-dev
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 100
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.8 100
-
-# Install swiftenv, which will take care of installing the appropriate Swift runtime
-if [ ! -d "$TROOT/.swiftenv" ]; then
-  git clone https://github.com/kylef/swiftenv $TROOT/.swiftenv
-fi
-export SWIFTENV_ROOT="$TROOT/.swiftenv"
-export PATH="$SWIFTENV_ROOT/bin:$PATH"
-eval "$(swiftenv init -)"
-
-# Install Swift runtime (but it's fine if it is already installed)
-swiftenv install || true
+fw_depends postgresql swift
 
 # Build Kitura
 #swift package clean
